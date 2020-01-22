9a9d0cd2951f91dadf945b6cea415f7008b3a9da
Felipe Araújo <fbma@cin.ufpe.br>
2020-01-22 14:22:51 -0300

    Iniciando o projeto

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes');
+const PORT = 3003;
+
+const app = express();
+
+
+app.use(express.json);
+app.use(routes);
+
+
+app.listen(PORT);
